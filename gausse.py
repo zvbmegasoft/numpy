72b9dd9af6f3a34eb211f9198306df69997e6405
zvb <zvbmegasoft@gmail.com>
2023-03-19 22:31:49 +0300

    метод Гаусса Маша лаб3 вар6

--- gausse.py
+++ gausse.py
@@ -1,7 +1,9 @@
 import numpy as np
-a = np.array([[1, 2], [3, 5]])
-b = np.array([1, 2])
+a = np.array([[13.21, 6.15, 7.11, 0.82],
+              [-10.15, -3.16, 2.22, -0.18],
+              [-7.10, 2.32, -0.91, 0.98],
+              [3.45, 16.21, 96.21, 103.22]])
+b = np.array([1.12, 2.45, 126.17, 0.15])
 x = np.linalg.solve(a, b)
-print(x)
 r = np.allclose(np.dot(a, x), b)
-print(r)
+print(x, r)
